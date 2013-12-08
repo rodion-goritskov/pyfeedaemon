3fe8e28c622eb3f72295a189d90e23abbd37617b
Rodion Goritskov <rodion.goritskov@gmail.com>
2013-12-08 23:39:42 +0400

    Some code style fixes. Code now looks much better (:

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from smtplib import *
 import email
 from email.mime.text import MIMEText
 
+
 class Feed:
-    def __init__(self,url,fd):
+    def __init__(self, url, fd):
         self.url = url
         self.fd = fd
         self.feed = feedparser.parse(self.url)
@@ -15,7 +16,11 @@ class Feed:
     def feed_write(self):
         self.fd.write('<h1>' + self.feed.feed.title + '</h1>\n')
         for self.i in self.feed.entries:
-            self.published_time = datetime(self.i.published_parsed[0],self.i.published_parsed[1],self.i.published_parsed[2],self.i.published_parsed[3],self.i.published_parsed[4])
+            self.published_time = datetime(self.i.published_parsed[0],
+                                           self.i.published_parsed[1],
+                                           self.i.published_parsed[2],
+                                           self.i.published_parsed[3],
+                                           self.i.published_parsed[4])
             print(self.published_time)
             if self.published_time >= config['time']:
                 self.flag = True
@@ -23,24 +28,29 @@ class Feed:
                 self.fd.write(self.i.published + '</br>')
                 self.fd.write(self.i.description + '</br>')
         return self.flag
-        
+
 
 def initFile():
-    '''Creates feed file and writes proper charser meta to it. Returns opened file. '''
-    fd = open('feed.html','w')
-    fd.write('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\n')
+    '''Creates feed file and writes proper charser meta to it.
+    Returns opened file. '''
+    fd = open('feed.html', 'w')
+    fd.write(
+        '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">\n')
     return fd
-    
+
 
 def openConfig():
-    '''Reads time from example.ini and writes new time of fetch to it. Creates new config file if current is empty
-    Returns time of the last fetch or current time if there is no config file.'''
+    '''Reads time from example.ini and writes new time of fetch to it.
+    Creates new config file if current is empty
+    Returns time of the last fetch
+    or current time if there is no config file.'''
 
     config = configparser.ConfigParser()
     curtime = str(datetime.utcnow())
-    conf = {'server': 0,'login': 0, 'password': 0, 'time': 0}
+    conf = {'server': 0, 'login': 0, 'password': 0, 'time': 0}
     if config.read('example.ini'):
-        conf['time'] = datetime.strptime(config['Last fetch']['Time'],'%Y-%m-%d %H:%M:%S.%f')
+        conf['time'] = datetime.strptime(config['Last fetch']['Time'],
+                                         '%Y-%m-%d %H:%M:%S.%f')
         config['Last fetch']['Time'] = curtime
         conf['server'] = config['Mail']['Server']
         conf['login'] = config['Mail']['Login']
@@ -53,16 +63,17 @@ def openConfig():
         config.write(configfile)
     return conf
 
+
 def sendEmail():
-    msg  = email.message.Message()
+    msg = email.message.Message()
     fp = open('feed.html', 'r')
-    msg = MIMEText(fp.read(),'html')
+    msg = MIMEText(fp.read(), 'html')
     fp.close
     msg['Subject'] = 'Дайджест новостей ' + datetime.now().strftime("%d %B %Y %H:%M")
-    mail = SMTP_SSL(config['server'],465)
-    mail.login(config['login'],config['password'])
-    mail.send_message(msg,'rodion.goritskov@gmail.com','rodion@goritskov.com')
-
+    mail = SMTP_SSL(config['server'], 465)
+    mail.login(config['login'], config['password'])
+    mail.send_message(msg, 'rodion.goritskov@gmail.com',
+                      'rodion@goritskov.com')
 
 
 fd = initFile()
@@ -70,13 +81,13 @@ config = openConfig()
 
 print(config['time'])
 
-habr = Feed('http://habrahabr.ru/rss/hubs/',fd)
-lor = Feed('http://feeds.feedburner.com/org/LOR',fd)
+habr = Feed('http://habrahabr.ru/rss/hubs/', fd)
+lor = Feed('http://feeds.feedburner.com/org/LOR', fd)
 
-feeds_list = {habr,lor}
+feeds_list = {habr, lor}
 for i in feeds_list:
     flag = i.feed_write()
 fd.close()
 
-if flag == True:
+if flag is True:
     sendEmail()
